45f261255b5f023a0193cb463d557cf88bb8bbae
procrastimax <heykeroth.dev@gmail.com>
2020-10-24 13:47:20 +0200

    Added icons, fixed disk usage

--- blocks.h
+++ blocks.h
@@ -3,10 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                                      /*Update Interval*/	/*Update Signal*/
     {"", "sh ~/programs/dwmblocks/scripts/connman_connection.sh",                     30,                   0},
     {"RAM ", "printf '%.2f%%' $(free | awk '/^Mem/ { x=$3; y=$7; printf x/y*100}')",  30,                   0},
-    {"DISK ", "df -t ext4 | grep '/' | awk '{print $5}'",                             60,		            0},
-	{"BAT ", "acpi -b | awk '{ print $4 $5 }' | sed 's/,/ /g'",                       30,		            0},
-	{"ALSA ", "sh ~/programs/dwmblocks/scripts/alsa_status.sh",                       0,		            10},
-	{"LIGHT ", "printf '%.0f%%' $(xbacklight)",                                       0,		            11},
+    {"💻 ", "printf '/: %s' $(df -t ext4 | grep '/$' | awk '{print $5}')",            60,		            0},
+	{"🔋 ", "acpi -b | awk '{ print $4 $5 }' | sed 's/,/ /g'",                        30,		            0},
+	{"🔊 ", "sh ~/programs/dwmblocks/scripts/alsa_status.sh",                         0,		            10},
+	{"☀ ", "printf '%.0f%%' $(xbacklight)",                                           0,		            11},
 	{"", "date '+%b %d. %a %H:%M:%S'",					                              5,            		0},
 };
 
