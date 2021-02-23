76978604860698fa0b0605be0a3937fe3b9c7c78
procrastimax <mmeheykeroth@protonmail.com>
2021-02-23 15:56:50 +0100

    Fixes RAM percentage calculation

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		                                                      /*Update Interval*/	/*Update Signal*/
     {"", "sh ~/programs/dwmblocks/scripts/connman_connection.sh",                     30,                   0},
-    {"RAM ", "printf '%.2f%%' $(free | awk '/^Mem/ { x=$3; y=$7; printf x/y*100}')",  30,                   0},
+    {"RAM ", "printf '%.1f%%' $(free -t | awk '/^Total/ { x=$3; y=$2; printf x/y*100}')",  30,                   0},
     {"", "printf '/: %s' $(df -t xfs | grep '/$' | awk '{print $5}')",                60,		            0},
 	{"🔋 ", "acpi -b | awk '{ print $4 $5 }' | sed 's/,/ /g'",                        30,		            0},
 	{"☀ ", "printf '%.0f%%' $(xbacklight -get)",                                      0,		            11},
